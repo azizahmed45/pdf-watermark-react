3a388e2016b71989961491a7f380e91f3f7247dd
Aziz Ahmed <azizahmedsabuj@gmail.com>
2020-07-08 23:36:14 +0600

    added default text options

--- src/components/PDFEditor.js
+++ src/components/PDFEditor.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import {
 	Grid,
@@ -32,7 +32,7 @@ import { SketchPicker } from 'react-color';
 function PDFEditor() {
 	const [ allFiles, setFiles ] = useState([]);
 	const [ text, setText ] = useState('Type Something');
-	const [ selected, setSelected ] = useState(0);
+	const [ selected, setSelected ] = useState();
 	const [ watermarkColor, setWatermarkColor ] = useState('#000000');
 	const [ watermarkPosition, setWatermarkPosition ] = useState({ x: 0, y: 50 });
 	const [ pageSize, setPageSize ] = useState({ width: 600, height: 600 });
@@ -41,6 +41,19 @@ function PDFEditor() {
 	const [ fontSize, setFontSize ] = useState(20);
 	const [ opacity, setOpacity ] = useState(1);
 	const [ angle, setAngle ] = useState(0);
+
+	const [ defaultText, setDefaultText ] = useState('Default text');
+	const [ defaultTextPosition, setDefaultTextPosition ] = useState({ x: 0, y: 0 });
+	const [ defaultTextColor, setDefaultTextColor ] = useState('#000000');
+	const [ defaultTextSize, setDefaultTextSize ] = useState(11);
+	const [ showDefaultTextColorPicker, setShowDefaultTextColorPicker ] = useState(false);
+
+	useEffect(
+		() => {
+			selectItem();
+		},
+		[ selected ]
+	);
 
 	const [ dropzone, setDropzone ] = useState({
 		open: false,
@@ -69,6 +82,12 @@ function PDFEditor() {
 			borderRadius: '2px',
 			background: `${watermarkColor}`
 		},
+		defaultTextColor: {
+			width: '64px',
+			height: '32px',
+			borderRadius: '2px',
+			background: `${defaultTextColor}`
+		},
 		swatch: {
 			padding: '5px',
 			background: '#fff',
@@ -96,21 +115,16 @@ function PDFEditor() {
 
 	function submitFile(files) {
 		handleDropzone();
-
 		setFiles(allFiles.concat(files));
-		selectItem(selected);
+
+		if (files.length > 0 && isNaN(selected)) {
+			setSelected(0);
+		}
 	}
 
-	function textChange(event) {
-		setText(event.target.value);
-	}
-
-	async function selectItem(index) {
-		setSelected(index);
-
-		generateColorForPdf();
+	async function selectItem() {
 		if (allFiles.length > 0) {
-			const buffer = await allFiles[index].arrayBuffer();
+			const buffer = await allFiles[selected].arrayBuffer();
 
 			const doc = await PDFDocument.load(buffer);
 
@@ -149,13 +163,22 @@ function PDFEditor() {
 			for (let page of doc.getPages()) {
 				const { x, y } = generatePosition();
 
+				const { defaultTextX, defaultTextY } = generateDefaultTextPosition();
+
 				page.drawText(text, {
 					x: x / ratio,
 					y: y / ratio,
 					opacity: opacity,
-					color: generateColorForPdf(),
+					color: generateColorForPdf(watermarkColor),
 					rotate: rotation,
 					size: fontSize / ratio
+				});
+
+				page.drawText(defaultText, {
+					x: defaultTextX / ratio,
+					y: defaultTextY / ratio,
+					color: generateColorForPdf(defaultTextColor),
+					size: defaultTextSize / ratio
 				});
 			}
 
@@ -178,10 +201,19 @@ function PDFEditor() {
 		};
 	}
 
-	function generateColorForPdf() {
-		let r = parseInt(watermarkColor.substring(1, 3), 16) / 255;
-		let g = parseInt(watermarkColor.substring(3, 5), 16) / 255;
-		let b = parseInt(watermarkColor.substring(5, 7), 16) / 255;
+	function generateDefaultTextPosition() {
+		let x = defaultTextPosition.x;
+		let y = pageSize.height - defaultTextPosition.y - defaultTextSize;
+		return {
+			defaultTextX: x,
+			defaultTextY: y
+		};
+	}
+
+	function generateColorForPdf(colorHex) {
+		let r = parseInt(colorHex.substring(1, 3), 16) / 255;
+		let g = parseInt(colorHex.substring(3, 5), 16) / 255;
+		let b = parseInt(colorHex.substring(5, 7), 16) / 255;
 
 		return rgb(r, g, b);
 	}
@@ -189,7 +221,7 @@ function PDFEditor() {
 	function getFiles() {
 		return allFiles.map((file, index) => {
 			return (
-				<ListItem key={index} divider button onClick={() => selectItem(index)} selected={selected === index}>
+				<ListItem key={index} divider button onClick={() => setSelected(index)} selected={selected === index}>
 					<ListItemAvatar>
 						<Avatar>
 							<PictureAsPdfRounded />
@@ -251,6 +283,22 @@ function PDFEditor() {
 								<Stage height={pageSize.height} width={pageSize.width}>
 									<Layer>
 										<Text
+											text={defaultText}
+											draggable
+											fontSize={defaultTextSize}
+											fill={defaultTextColor}
+											x={defaultTextPosition.x}
+											y={defaultTextPosition.y}
+											onDragEnd={(e) => {
+												setDefaultTextPosition({
+													x: e.target.x(),
+													y: e.target.y()
+												});
+											}}
+										/>
+									</Layer>
+									<Layer>
+										<Text
 											text={text}
 											draggable
 											fontSize={fontSize}
@@ -290,9 +338,61 @@ function PDFEditor() {
 								</FormControl>
 							</Grid> */}
 
-							<Grid item>
+							<Grid item style={{ backgroundColor: '#AAAAAA' }}>
+								<Typography variant="h6">Default Text Size</Typography>
+								<TextField
+									size="small"
+									variant="outlined"
+									value={defaultTextSize}
+									onChange={(event) => {
+										let number = Number(event.target.value);
+
+										if (!isNaN(number) && number < 101) {
+											setDefaultTextSize(number);
+										}
+									}}
+								/>
+
+								<Typography variant="h6">Default text color </Typography>
+								<div>
+									<div
+										className={colorPickerCss.swatch}
+										onClick={() => {
+											setShowDefaultTextColorPicker(!showDefaultTextColorPicker);
+										}}
+									>
+										<div className={colorPickerCss.defaultTextColor} />
+									</div>
+									{showDefaultTextColorPicker ? (
+										<div className={colorPickerCss.popover}>
+											<div
+												className={colorPickerCss.cover}
+												onClick={() => {
+													setShowDefaultTextColorPicker(!showDefaultTextColorPicker);
+												}}
+											/>
+											<SketchPicker
+												color={defaultTextColor}
+												onChange={(color) => setDefaultTextColor(color.hex)}
+											/>
+										</div>
+									) : null}
+								</div>
+
+								<TextField
+									size="small"
+									variant="outlined"
+									value={defaultText}
+									placeholder="Text to show"
+									multiline
+									onChange={(event) => setDefaultText(event.target.value)}
+								/>
+							</Grid>
+
+							<Grid item style={{ backgroundColor: '#898989' }}>
 								<Typography variant="h6">Watermark SIze</Typography>
 								<TextField
+									size="small"
 									variant="outlined"
 									value={fontSize}
 									onChange={(event) => {
@@ -331,48 +431,49 @@ function PDFEditor() {
 								</div>
 
 								<TextField
+									size="small"
 									variant="outlined"
 									value={text}
 									placeholder="Text to show"
 									multiline
-									onChange={textChange}
+									onChange={(event) => setText(event.target.value)}
 								/>
+
+								<Typography variant="h6">Opacity</Typography>
+								<Slider
+									value={opacity}
+									min={0.05}
+									step={0.001}
+									max={1}
+									onChange={(event, value) => {
+										setOpacity(value);
+									}}
+								/>
+
+								<Typography variant="h6">Rotation</Typography>
+								<Slider
+									value={angle}
+									min={0}
+									step={0.001}
+									max={360}
+									onChange={(event, value) => {
+										setAngle(value);
+									}}
+								/>
+
+								<Box marginTop={3}>
+									<Button
+										startIcon={<CloudDownload />}
+										fullWidth
+										variant="contained"
+										color="primary"
+										onClick={process}
+									>
+										Save PDF
+									</Button>
+								</Box>
 							</Grid>
 						</Grid>
-
-						<Typography variant="h6">Opacity</Typography>
-						<Slider
-							value={opacity}
-							min={0.05}
-							step={0.001}
-							max={1}
-							onChange={(event, value) => {
-								setOpacity(value);
-							}}
-						/>
-
-						<Typography variant="h6">Rotation</Typography>
-						<Slider
-							value={angle}
-							min={0}
-							step={0.001}
-							max={360}
-							onChange={(event, value) => {
-								setAngle(value);
-							}}
-						/>
-
-						<Box marginTop={3}>
-							<Button
-								startIcon={<CloudDownload />}
-								fullWidth
-								variant="contained"
-								color="primary"
-								onClick={process}
-							>
-								Save PDF
-							</Button>
-						</Box>
 					</Grid>
 				</Grid>
 			</Grid>
